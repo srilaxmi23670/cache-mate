323d67b98b5aa21ab01502c887f570ed1c6cc966
Srilaxmi <srilaxmi.n@turtlemint.com>
2024-07-03 23:00:38 +0530

    refactor name

--- src/main/java/com/github/srilaxmi/cache/CacheMateApplication.java
+++ src/main/java/com/github/srilaxmi/cache/CacheMateApplication.java
@@ -4,7 +4,7 @@ package com.github.srilaxmi.cache;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CacheMate {
+public class CacheMateApplication {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
